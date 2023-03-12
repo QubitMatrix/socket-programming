fb09e5c8ce10c457297a68aacfe3d66b8272e5ed
QubitMatrix <preethi13m@gmail.com>
2023-03-12 12:10:30 +0530

    combined client code of both games

--- client.py
+++ client.py
@@ -1,15 +1,48 @@
 from socket import*
-serverName='172.20.117.235'
+from count1 import *
+serverName='172.21.248.65'
 serverPort=12002
 clientSocket=socket(AF_INET,SOCK_DGRAM)
 while(True):
-    message=input('Enter your guess:')
-    clientSocket.sendto(message.encode(),(serverName,serverPort))
-    modMsg,serverAddress=clientSocket.recvfrom(2048)
-    #print(serverAddress)
-    modMsg=modMsg.decode()
-    if(modMsg=="end"):
-        break
-    print(modMsg)
-clientSocket.close()
+    def game1():
+        while (True):
+            message = input('Enter your guess:')
+            clientSocket.sendto(message.encode(), (serverName, serverPort))
+            modMsg, serverAddress = clientSocket.recvfrom(2048)
+            # print(serverAddress)
+            modMsg = modMsg.decode()
+            if (modMsg == "end"):
+                break
+            print(modMsg)
+    def game2():
+        print("Score format: You-Computer")
+        while(True):
+            message=get_count()
+            message=str(message)
+            clientSocket.sendto(message.encode(),(serverName,serverPort))
+            modMsg,serverAddress=clientSocket.recvfrom(2048)
+            print(serverAddress)
+            modMsg=modMsg.decode()
+            print(modMsg)
+            if("Game" in modMsg):
+                choice=input("end or no?")
+                clientSocket.sendto(choice.encode(),(serverName,serverPort))
+                if(choice=="end"):
+                    break
 
+
+    choice = input("Enter the choice(1 or 2 or 3)")
+    if (choice == '3'):
+        break
+    clientSocket.sendto(choice.encode(), (serverName, serverPort))
+    while (True):  # waiting for acknowledgement before starting game
+        modMsg, serverAddress = clientSocket.recvfrom(2048)
+        modMsg = modMsg.decode()
+        print(modMsg)
+        if ("scissor" in modMsg):
+            game2()
+            break
+        if ("hangman" in modMsg):
+            game1()
+            break
+clientSocket.close()
