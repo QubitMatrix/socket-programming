f3d2ffdcab0c5c2416054c8650913716e03c4160
QubitMatrix <preethi13m@gmail.com>
2023-03-23 20:50:34 +0530

    migrated server code from udp to tcp connection

--- server.py
+++ server.py
@@ -1,39 +1,41 @@
 from socket import *
 from hangman import *
 from sps import *
-serverPort=12002
-serverSocket=socket(AF_INET,SOCK_DGRAM)
+serverPort=12000
+serverSocket=socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
 print("Server ready")
-def game1(clientAddress):
+def game1(connectionSocket,clientAddress):
     count=0
     word,hint=choose_word()
     ans=""
     print("hangman",clientAddress)
-    serverSocket.sendto("Get ready to play hangman!!!".encode(),clientAddress)
+    connectionSocket.send("Get ready to play hangman!!!".encode())
     while True:
-        message,clientAddress=serverSocket.recvfrom(2048)
+        message=connectionSocket.recv(2048)
         print(clientAddress)
         if("You won" in ans or "You lose" in ans):
           word,hint=choose_word()
         modMsg=message.decode().upper()
         if(modMsg.lower()=="end"):
-          serverSocket.sendto(modMsg.lower().encode(),clientAddress)
+          connectionSocket.send(modMsg.lower().encode())
+          connectionSocket.close()
           break
         ans,count=hangman_fun(word,hint,modMsg,count)
         ans="".join(ans)
         print("in server",ans,count)
-        serverSocket.sendto(ans.encode(),clientAddress)
-def game2(clientAddress):
+        connectionSocket.send(ans.encode())
+def game2(connectionSocket,clientAddress):
     count=0
     comp=0
     player=0
     player_choice=""
     counter=0
     print("SPS")
-    serverSocket.sendto("Get ready to play stone,paper,scissor".encode(),clientAddress)
+    connectionSocket.send("Get ready to play stone,paper,scissor".encode())
     while True:
-        count,clientAddress=serverSocket.recvfrom(2048)
+        count=connectionSocket.recv(2048)
         count=count.decode()
         print(count)
         count=int(count)
@@ -51,7 +53,7 @@ def game2(clientAddress):
         print("in server",player,count,comp,comp_choice)
         ans="Score:"+str(player)+"-"+str(comp)+"\n("+player_choice+","+comp_choice+")"
         counter+=1
-        if(counter==7):
+        if(counter==3):
             counter=0
             if (player==comp):
                 winner="Draw"
@@ -60,18 +62,21 @@ def game2(clientAddress):
             comp=0
             player=0
             ans=ans+"\nGame over..."+winner
-            serverSocket.sendto(ans.encode(),clientAddress)
-            msg,clientAddress=serverSocket.recvfrom(2048)
+            connectionSocket.send(ans.encode())
+            msg=connectionSocket.recv(2048)
             if(msg.decode().lower()=='end'):
+                connectionSocket.close()
                 break
         else:
-            serverSocket.sendto(ans.encode(),clientAddress)
+            connectionSocket.send(ans.encode())
 while(True): #to choose which game
-    choice,clientAddress=serverSocket.recvfrom(2048)
+    connectionSocket,clientAddress=serverSocket.accept()
+    choice=connectionSocket.recv(2048)
     choice=choice.decode()
     print(choice)
     if(choice=='1'):
-        game1(clientAddress)
+        game1(connectionSocket,clientAddress)
     elif (choice=='2'):
-        game2(clientAddress)
+        game2(connectionSocket,clientAddress)
     choice=0
+    #connectionSocket.close()
